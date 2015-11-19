26c52747eb3a114ec254757168b19a52eb6da056
James Burnell <jamesburnell@Jamess-MacBook-Pro.local>
2015-11-19 15:30:53 -0800

    server works

--- client/main.jsx
+++ client/main.jsx
@@ -1,30 +1,19 @@
 var React = require('react');
-var App = require('./components/app.js');
 var ReactDOM = require('react-dom');
 
-var Router = require('react-router').Router;
-var Route = require('react-router').Route;
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
 var IndexRoute = require('react-router').IndexRoute;
 var Link = require('react-router').Link;
 
-var Landing = require('./components/landingComponents/landing');
-var MainRoom = require('./components/mainRoomComponent');
+var history = require('history').createBrowserHistory
 
+// grab our routes from the routes.js folder
+var routes = require('./routes');
 
-// react component routes!
+// render to DOM with the imported routes (this is the react router v1.0 way, different from old react router)
+ReactDOM.render(<Router history={history}>{routes}</Router>, document.getElementById('appContainer'));
 
-ReactDOM.render(
-			
-				<Router>
-					<Route path="/" component={App}>
-						// This route shows up first
-						<IndexRoute component={Landing} />
-						// on submit/button click, this route shows up
-						<Route path="main" component={MainRoom} />
-
-					</Route>
-				</Router>
-
-			, document.getElementById('appContainer')
-			);
+	
 
--- server/server.js
+++ server/server.js
@@ -1,5 +1,25 @@
 var express = require('express');
+var path = require('path');
 var React = require('react');
-var Router = require('react-router');
+var ReactRouter = require('react-router');
+var Router = ReactRouter.Router;
+var history = require('history').createBrowserHistory
+
+var ReactDOMServer = require('react-dom/server');
 
 var app = express();
+
+app.use(express.static(path.join(__dirname + './../')))
+// var routes = require('../client/routes');
+
+// app.use(function(req, res) {
+// 	ReactDOMServer.renderToString(<Router routes={routes} />)
+// })
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + 'index.html'))
+})
+
+app.listen(3100, function(){
+	console.log("server is up and runnin' ...");
+});
