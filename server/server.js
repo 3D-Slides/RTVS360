408addc10cb2f4a3dec3ad8d213795f5d575d280
William Gottschalk <will.gottschalk@gmail.com>
2015-12-17 11:39:58 -0800

    removed ReactDOMserver form server.js

--- server/server.js
+++ server/server.js
@@ -4,27 +4,25 @@ var request = require('request');
 var path = require('path');
 var bodyParser = require('body-parser');
 
-var ReactDOMServer = require('react-dom/server');
-
 var app = express();
 
 app.use(bodyParser.text({ type: 'text/html' }));
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname + './../')))
+app.use(express.static(path.join(__dirname + './../')));
 
 app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + './index.html'))
-})
+	res.sendFile(path.join(__dirname + './index.html'));
+});
 
 app.get('/presentation', function(req, res) {
 	res.sendFile(path.join(__dirname +'./../presentation.html'));
-})
+});
 
 app.post('/presentation', function(req, res) {
 	res.send();
-})
+});
 
 app.listen(process.env.PORT || 3131, function(){
 	console.log("Yo fool! The server is connected on port 3131! ...");
