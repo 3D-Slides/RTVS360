604403a01ac69a9d36be5620a415f1361cfa05e9
jshuadvd <jshuadvd@icloud.com>
2015-12-29 21:45:46 -0800

    Uglify Bundles works, but site is broken

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,88 +9,94 @@ var glob = require('glob');
 var streamify = require('gulp-streamify');
 
 gulp.task('watchify', function () {
-	var bundler = browserify({
-		entries: ['./client/main.jsx'],
-		transform: [reactify],
-		debug: true,
-		cache: {},
-		packageCache: {},
-		fullPaths: true
-	});
+    var bundler = browserify({
+        entries: ['main.jsx'],
+        transform: [reactify],
+        debug: true,
+        cache: {},
+        packageCache: {},
+        fullPaths: true
+    });
 
-	var watcher = watchify(bundler);
+    var watcher = watchify(bundler);
 
-	return watcher
-		.on('update', function () {
-			watcher.bundle()
-				.on('error', function (err) {
-					console.log('There was an error compiling the components.', err.message);
-				})
-				.pipe(source('bundle.js'))
-				.pipe(gulp.dest('./dest/'));
-		})
-		.bundle()
-		.on('error', function (err) {
-			console.log('There was an error compiling the components.', err.message);
-		})
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest('./dest/'));
+    return watcher
+        .on('update', function () {
+            watcher.bundle()
+                .on('error', function (err) {
+                    console.log('There was an error compiling the components.', err.message);
+                })
+                .pipe(source('bundle.js'))
+                .pipe(streamify(uglify()))
+                .pipe(gulp.dest('./dest/'));
+        })
+        .bundle()
+        .on('error', function (err) {
+            console.log('There was an error compiling the components.', err.message);
+        })
+        .pipe(source('bundle.js'))
+        .pipe(streamify(uglify())) // wrap uglify with the streamify plugin
+        .pipe(gulp.dest('./dest/'));
 });
 
 gulp.task('browserify', function () {
-	var testFiles = glob.sync('./client/main.jsx');
-	var bundler = browserify({
-		entries: testFiles,
-		transform: [reactify],
-		debug: true,
-		cache: {},
-		packageCache: {},
-		fullPaths: true
-	});
+  var testFiles = glob.sync('./src/main.js');
 
-	function rebundle() {
-		bundler
-		.bundle()
-		.on('error', function(err) {
-			console.log('There was an error compiling the components.', err.message);
-		})
-		.pipe(source('bundle.js'))
-		.pipe(gulp.dest('./dest/'));
-	}
-	return rebundle();
+  var bundler = browserify({
+    entries: testFiles,
+    transform: [reactify],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  });
+
+  function rebundle() {
+    bundler
+    .bundle()
+    .on('error', function(err) {
+      console.log('There was an error compiling the components.', err.message);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('./dest/'));
+  }
+
+  rebundle();
 });
 
 gulp.task('home', function () {
+  var testFiles = glob.sync('./src/main2.js');
 
-	var testFiles = glob.sync('./client/homepage.jsx');
-	var bundler = browserify({
-		entries: testFiles,
-		transform: [reactify],
-		debug: true,
-		cache: {},
-		packageCache: {},
-		fullPaths: true
-	});
+  var bundler = browserify({
+    entries: testFiles,
+    transform: [reactify],
+    debug: true,
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  });
 
-	function rebundle() {
-		bundler
-		.bundle()
-		.on('error', function(err) {
-			console.log('There was an error compiling the components.', err.message);
-		})
-		.pipe(source('homeBundle.js'))
-		.pipe(gulp.dest('./dest/'));
-	}
+  function rebundle() {
+    bundler
+    .bundle()
+    .on('error', function(err) {
+      console.log('There was an error compiling the components.', err.message);
+    })
+    .pipe(source('homeBundle.js'))
+    .pipe(streamify(uglify()))
+    .pipe(gulp.dest('./dest/'));
+  }
 
-	return rebundle();
+  rebundle();
 });
 
 
 // Uglify Scripts
 gulp.task('scripts', function() {
-	gulp.src('assets/js/*.js')
+  gulp.src('assets/js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest('dist/js'))
+    .pipe(gulp.dest('./dest/js'));
 });
 
 // Minify Styles
@@ -99,7 +105,7 @@ gulp.task('styles', function() {
     .pipe(uglifycss({
       "max-line-len": 80
     }))
-    .pipe(gulp.dest('./dist/css'));
+    .pipe(gulp.dest('./dest/css'));
 });
 
 
